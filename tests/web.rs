e463e7edefe8860505600f27feb5ca78dd4231bb
Guanzhou Hu <huguanzhou123@sina.com>
2022-09-24 23:25:47 -0500

    game of life demo done

--- tests/web.rs
+++ tests/web.rs
@@ -2,12 +2,39 @@
 
 #![cfg(target_arch = "wasm32")]
 
-extern crate wasm_bindgen_test;
-use wasm_bindgen_test::*;
+use std::assert_eq;
 
-wasm_bindgen_test_configure!(run_in_browser);
+use wasm_bindgen_test::*;
+use bitaggy::Universe;
+
+
+// Uncomment below to force run tests with browser drivers.
+// wasm_bindgen_test_configure!(run_in_browser);
+
+
+#[cfg(test)]
+pub fn input_spaceship() -> Universe {
+    let mut universe = Universe::new(6, 6, false);
+    universe.set_cells(&[(1,2), (2,3), (3,1), (3,2), (3,3)]);
+    universe
+}
+
+#[cfg(test)]
+pub fn expected_spaceship() -> Universe {
+    let mut universe = Universe::new(6, 6, false);
+    universe.set_cells(&[(2,1), (2,3), (3,2), (3,3), (4,2)]);
+    universe
+}
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn test_tick_spaceship() {
+    let mut input_universe = input_spaceship();
+    let expected_universe = expected_spaceship();
+    input_universe.tick();
+    assert_eq!(input_universe.width(), expected_universe.width());
+    assert_eq!(input_universe.height(), expected_universe.height());
+    let input_cells = input_universe.get_cells();
+    let expected_cells = expected_universe.get_cells();
+    assert!(input_cells.is_subset(expected_cells)
+            && input_cells.is_superset(expected_cells));
 }
