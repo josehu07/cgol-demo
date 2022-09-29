6723395e4d2ef1144d6b9d1cb8a522b122acf1b6
Guanzhou Hu <huguanzhou123@sina.com>
2022-09-28 20:30:29 -0500

    backend selection page styling

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 /// Sets the panic hook to browser web console for easier debugging.
+#[allow(dead_code)]
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
---
+++ www-bitaggy/index.js
@@ -0,0 +1,10 @@
+import { Universe } from "bitaggy";
+import { memory } from "bitaggy/bitaggy_bg";
+
+
+// Load in Dropbox API SDK.
+var Dropbox = require("dropbox").Dropbox;
+var dbx = new Dropbox({ accessToken: "xxxxx" });
+
+// var folders = dbx.filesListFolder({ path: "" });
+
